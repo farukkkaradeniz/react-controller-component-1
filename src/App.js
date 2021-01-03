38259781c35a7773c6469480137ae775b700ca52
TCFKARADENIZ <faruk.karadeniz@turkcell.com.tr>
2021-01-03 16:58:51 +0300

    exercise completed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message : ''
+    };
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      message : event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,9 +25,15 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <div className="container">
-          <input type="text" placeholder="Say Something" />
-          <p className="echo">Echo:</p>
-          <p>This should mirror the text you typed into the input field.</p>
+          <input type="text" placeholder="Say Something"  value={this.state.message} onChange={this.handleChange} />
+          {
+            this.state.message.length > 0 &&( // burada önemli olan && işareti ile jsx içinde if block kullanabilmek.
+              <div>
+                <p className="echo">Echo:{this.state.message}</p>
+                <p>This should mirror the text you typed into the input field.</p>
+              </div>
+            )
+          }
         </div>
       </div>
     );
